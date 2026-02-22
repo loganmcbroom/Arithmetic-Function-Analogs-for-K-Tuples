25f9a221f6bd2077048ee56f35a98dfac6019642
Logan McBroom <loganmcbroom@gmail.com>
2026-02-21 19:30:05 -0500

    Fixed massive blunder in modulator constants section, updated with new computations, conjecture, constant equality result.

--- Computation.py
+++ Computation.py
@@ -1,7 +1,7 @@
 import math
 import cmath
 
-bound = 256
+bound = 104729+1
 
 def divisors(n):
     n = abs(n)
@@ -59,7 +59,7 @@ N = range(bound)
 phi = conv( mu, N )
 
 from collections import defaultdict
-def u_gen(A): # A is a constellation (as a set)
+def unit_gen(A, r):
     def factor(n: int) -> dict[int, int]:
         factors = defaultdict(int)
         while n % 2 == 0:
@@ -74,15 +74,19 @@ def u_gen(A): # A is a constellation (as a set)
         if n > 1:
             factors[n] += 1
         return dict(factors)
-    def r(p): # u for primes
-        A_mod_p = set()
-        for k in A:
-            A_mod_p.add( k % p )
-        return len(A_mod_p)
     def out(n):
         if n < 1: return 0
-        return math.prod(r(p)**e for [p,e] in factor(n).items())
+        return math.prod(r(p, A)**e for [p,e] in factor(n).items())
     return eval(out)
+def r(p, A): # u for primes
+    A_mod_p = set()
+    for k in A:
+        A_mod_p.add( k % p )
+    return len(A_mod_p)
+def u_gen(A):
+    return unit_gen( A, r )
+def w_gen(A):
+    return unit_gen( A, lambda p : r(p)/len(A) )
 
 def mu_gen(A):
     return pointwise(u_gen(A), mu)
@@ -90,39 +94,40 @@ def mu_gen(A):
 def phi_gen(A):
     return conv(N, mu_gen(A))
 
-A = set([0,2,6])
-u_A = u_gen(A)
-mu_A = mu_gen(A)
-phi_A = phi_gen(A)
-def is_square(n):
-    return int(math.sqrt(n)) ** 2 == n
-sq = [1 if is_square(i) else 0 for i in range(bound)]
-la = conv(sq,mu)
+# A = set([0,2,6])
+# u_A = u_gen(A)
+# w_A = w_gen(A)
+# mu_A = mu_gen(A)
+# phi_A = phi_gen(A)
+# def is_square(n):
+#     return int(math.sqrt(n)) ** 2 == n
+# sq = [1 if is_square(i) else 0 for i in range(bound)]
+# la = conv(sq,mu)
 
 # Dirichlet character extension testing
-w = cmath.exp((2*math.pi / 6.0)*1j)
-w2 = w**2
-chis = [
-    [0,1,0,  1,0,  1,0,0,0,  1,0,  1,0,  1]*64,
-    [0,1,0,  w,0,-w2,0,0,0, w2,0, -w,0, -1]*64,
-    [0,1,0, w2,0, -w,0,0,0, -w,0, w2,0,  1]*64,
-    [0,1,0, -1,0, -1,0,0,0,  1,0,  1,0, -1]*64,
-    [0,1,0, -w,0, w2,0,0,0, w2,0, -w,0,  1]*64,
-    [0,1,0,-w2,0,  w,0,0,0, -w,0, w2,0, -1]*64,
-    ]
-chi_As = [pointwise(chi,u_A) for chi in chis]
+# w = cmath.exp((2*math.pi / 6.0)*1j)
+# w2 = w**2
+# chis = [
+#     [0,1,0,  1,0,  1,0,0,0,  1,0,  1,0,  1]*64,
+#     [0,1,0,  w,0,-w2,0,0,0, w2,0, -w,0, -1]*64,
+#     [0,1,0, w2,0, -w,0,0,0, -w,0, w2,0,  1]*64,
+#     [0,1,0, -1,0, -1,0,0,0,  1,0,  1,0, -1]*64,
+#     [0,1,0, -w,0, w2,0,0,0, w2,0, -w,0,  1]*64,
+#     [0,1,0,-w2,0,  w,0,0,0, -w,0, w2,0, -1]*64,
+#     ]
+# chi_As = [pointwise(chi,u_A) for chi in chis]
 
 # Omega testing
-def Omega(n):
-    O = 0
-    d = 2
-    while d <= n:
-        while n % d == 0:
-            O += 1
-            n //= d
-        d += 1
-    return O
-c_Omega = eval( lambda n : len(A)**Omega(n) )
+# def Omega(n):
+#     O = 0
+#     d = 2
+#     while d <= n:
+#         while n % d == 0:
+#             O += 1
+#             n //= d
+#         d += 1
+#     return O
+# c_Omega = eval( lambda n : len(A)**Omega(n) )
 
 # f = eval(lambda n : c_Omega[n]/u_A[n])
 # for n in range(1,20):
@@ -135,7 +140,9 @@ c_Omega = eval( lambda n : len(A)**Omega(n) )
 # That also gives L(s,la_A) = Prod 1/(1+u_A(p)p^(-s))
 # la_A = conv(pointwise( sq, u_A ), mu_A)
 # I proved this is equal to the following:
-la_A = pointwise( la, u_A )
+# la_A = pointwise( la, u_A )
+
+# print( conv(w_A, mu) )
 
 
 
--- Modulator_calc.py
+++ Modulator_calc.py
@@ -1,9 +1,9 @@
-import Computation
 import math
 from fractions import Fraction
 import pandas as pd
+from primes import primes
 
-primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71]
+primes_max = 1000000
 
 kts = [
     [0],
@@ -16,42 +16,60 @@ kts = [
     [0,4,10],
     [0,4,28],
     [0,2,6,8],
-    [0,2,6,30],
+    [0,18,30,60],
+    [0,60,120,180],
+    [0,2,6,8,30],
+    
+    # [0,2,6,8],
+    # [0,6,12,18],
+    # [0,2,6,30],
+    # [0,120,122,126],
+    # [0,8,120,180],
+    # [0,12,22,42],
+    # [0,6,30,42],
+
     ]
 
-# Compute u_As, we only need primes but that's alright
-uAs = [Computation.u_gen(A) for A in kts]
+# Compute residue counts
+def r_gen(A): # u for primes
+    def out(p):
+        A_mod_p = set()
+        for k in A:
+            A_mod_p.add( k % p )
+        return len(A_mod_p)
+    return out
+rAs = [r_gen(A) for A in kts]
 
 # Find modulated primes for each uA
-def find_mods(A, uA):
+def find_mods(A, rA):
     mods = []
-    for p in primes:
-        if uA[p] < len(A):
+    for p in primes[:primes_max]:
+        if rA(p) < len(A):
             mods.append(p)
     return mods
-mods = [find_mods(kts[i], uAs[i]) for i in range(len(kts))]
+mods = [find_mods(kts[i], rAs[i]) for i in range(len(kts))]
 
 # Compute M_A(1)
-def compute_M_A(A, uA, mod):
+def compute_M_A(A, rA, mod):
     c = len(A)
-    return math.prod( (1-Fraction(1,p)) / (1-Fraction(uA[p],p*c)) for p in mod )
-M_As = [compute_M_A(kts[i], uAs[i], mods[i]) for i in range(len(kts))]
+    return math.prod( (1-Fraction(1,p)) / (1-Fraction(rA(p),p*c)) for p in mod )
+M_As = [compute_M_A(kts[i], rAs[i], mods[i]) for i in range(len(kts))]
 
 # Compute N_A(1)
-def compute_N_A(A, uA, mod):
+def compute_N_A(A, rA):
     c = len(A)
-    return math.prod( (1-Fraction(1,p))**c / (1-Fraction(uA[p],p)) for p in mod )
-N_As = [compute_N_A(kts[i], uAs[i], mods[i]) for i in range(len(kts))]
+    return math.prod( (1-1/p)**c / (1-rA(p)/p) for p in primes[:primes_max] )
+N_As = [compute_N_A(kts[i], rAs[i]) for i in range(len(kts))]
 
 
 # Print everything 
+pd.set_option("display.float_format", "{:.8f}".format)
 print()
 print(pd.DataFrame({
     "k-tuple"   : kts,
     "modulates" : mods,
-    "M_A"       : M_As,
+    "M_A frac"  : M_As,
     "M_A dec"   : [float(M) for M in M_As],
-    "N_A"       : N_As,
-    "N_A dec"   : [float(N) for N in N_As],
+    "N_A"       : [float(N) for N in N_As],
 }))
-print()
+print()  
